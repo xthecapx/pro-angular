7c81f603d33b1b40b4ccd4cc6d2e1d3a2096316e
The Cap <xthecapx@gmail.com>
2017-12-31 13:34:16 -0500

    Added cart-summary

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
+import { Cart } from "./cart.model";
 
 @NgModule({
   imports: [
@@ -9,7 +10,8 @@ import { StaticDataSource } from "./static.datasource";
   ],
   providers: [
     ProductRepository,
-    StaticDataSource
+    StaticDataSource,
+    Cart
   ],
   declarations: []
 })
--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
 
 @Component({
   selector: 'app-store',
@@ -12,7 +13,8 @@ export class StoreComponent implements OnInit {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) { }
+  constructor(private repository: ProductRepository,
+              private cart: Cart) { }
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -47,7 +49,10 @@ export class StoreComponent implements OnInit {
     return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
   }
 
-  ngOnInit() {
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 
+  ngOnInit() {
+  }
 }
