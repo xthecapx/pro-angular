96f72b69acd205165819b24fce356c04e4bff211
The Cap <xthecapx@gmail.com>
2017-12-31 15:26:20 -0500

    Updated data to use the mock server

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -1,21 +1,28 @@
+import { HttpModule } from "@angular/http";
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
+import { RestDataSource } from "./rest.datasource";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpModule
   ],
   providers: [
     ProductRepository,
-    StaticDataSource,
     Cart,
     Order,
-    OrderRepository
+    OrderRepository,
+    {
+      provide: StaticDataSource,
+      useClass: RestDataSource
+    }
   ],
   declarations: []
 })
