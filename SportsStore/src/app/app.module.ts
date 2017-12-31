b2c22f23f058580e249f72d5cfb96ad9ec04c507
The Cap <xthecapx@gmail.com>
2017-12-31 12:02:06 -0500

    Added directive, complete project

--- SportsStore/src/app/app.module.ts
+++ SportsStore/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreComponent } from './store/store.component';
+import { StoreModule } from "./store/store.module";
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StoreComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -8,15 +8,44 @@ import { ProductRepository } from "../model/product.repository";
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+  public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage)
   }
 
   ngOnInit() {
