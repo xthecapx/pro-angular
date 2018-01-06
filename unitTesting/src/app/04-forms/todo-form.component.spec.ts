858498e7c7ff1c77e4bff7a2b1176780b37c7b36
The Cap <xthecapx@gmail.com>
2018-01-06 16:30:35 -0500

    Exercise 04 solved

--- unitTesting/src/app/04-forms/todo-form.component.spec.ts
+++ unitTesting/src/app/04-forms/todo-form.component.spec.ts
@@ -1,17 +1,23 @@
+import { FormBuilder } from '@angular/forms'
 import { TodoFormComponent } from './todo-form.component';
 
 describe('TodoFormComponent', () => {
   var component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
-
+  it('Should create a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
   });
 
-  it('', () => {
+  it('Should make the name control required', () => {
+    let control = component.form.get('name');
 
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
   });
 });
--- unitTesting/src/app/04-forms/todo-form.component.ts
+++ unitTesting/src/app/04-forms/todo-form.component.ts
@@ -4,6 +4,9 @@ export class TodoFormComponent {
   form: FormGroup;
 
   constructor(fb: FormBuilder) {
+    //We need to test:
+    // 1. 1 Form group and 2 forms controls called 'name' and 'email'
+    // 2. Name control is required
     this.form = fb.group({
       name: ['', Validators.required],
       email: [''],
