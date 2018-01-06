93093ae402b7a439d40528d425f6f3b3b092cc60
The Cap <xthecapx@gmail.com>
2018-01-06 16:14:47 -0500

    Exercise 03 solved

--- unitTesting/src/app/03-firts-angular-component/vote.component.spec.ts
+++ unitTesting/src/app/03-firts-angular-component/vote.component.spec.ts
@@ -1,9 +1,40 @@
 import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  // Setup function
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  // afterEach(() => {}); // Good place for cleanup something
+  // beforeAll(() => {});
+  // afterAll(() => {}); // Tear down
+
+  it('Should increment totalVotes when upvoted', () => {
+    // Pattern -> ARRANGE ACT ASSERT
+    // ARRANGE -> Create an instace of the component. Initialice everything
+    // Use empty lines between each block
+    // let component = new VoteComponent();
+
+    // ACT -> call a method
+    component.upVote();
+
+    // ASSERT -> Expected results
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('Should decrement totalVotes when downvotes', () => {
+    // Pattern -> ARRANGE ACT ASSERT
+    // ARRANGE -> Create an instace of the component. Initialice everything
+    // Use empty lines between each block
+    // let component = new VoteComponent(); // Moved beforeEach
+
+    // ACT -> call a method
+    component.downVote();
+
+    // ASSERT -> Expected results
+    expect(component.totalVotes).toBe(-1);
   });
 });
