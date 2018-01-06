3ea7113c10bddbfcebdfa57b4e5cca8fadb306e2
The Cap <xthecapx@gmail.com>
2018-01-06 16:35:11 -0500

    Added Exercise 05

---
+++ unitTesting/src/app/05-event-emitters/vote.component.spec.ts
@@ -0,0 +1,13 @@
+import { VoteComponent } from './vote.component';
+
+describe('VoteComponent', () => {
+  var component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+
+  it('', () => {
+
+  });
+});
