866eadadac385be0d9d187c004c36dab7fe0e86a
The Cap <xthecapx@gmail.com>
2018-01-06 16:44:46 -0500

    Exercise 05 completed

--- unitTesting/src/app/05-event-emitters/vote.component.spec.ts
+++ unitTesting/src/app/05-event-emitters/vote.component.spec.ts
@@ -7,7 +7,18 @@ describe('VoteComponent', () => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('Should raise voteChanged event when upvoted', () => {
+    // ARRANGE
+    let totalVotes = null;
+    component.voteChanged.subscribe(data => {
+      totalVotes = data;
+    });
 
+    // ACT
+    component.upVote();
+
+    //ASSERT
+    // expect(totalVotes).not.toBeNull(); // Works but we can be more specifid
+    expect(totalVotes).toBe(1);
   });
 });
--- unitTesting/src/app/05-event-emitters/vote.component.ts
+++ unitTesting/src/app/05-event-emitters/vote.component.ts
@@ -2,8 +2,11 @@ import { EventEmitter } from '@angular/core';
 
 export class VoteComponent {
   totalVotes = 0;
-  voteChanged = new EventEmitter();
+  voteChanged = new EventEmitter(); // Usually as @Output
 
+  // We should test
+  // 1. Votes is increased.
+  // 2. An event is emited with the vote increased
   upVote() {
     this.totalVotes++;
     this.voteChanged.emit(this.totalVotes);
