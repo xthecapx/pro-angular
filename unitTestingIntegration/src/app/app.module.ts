e21c4bfafd578b704f0e4f8497b4c9443e6bcab7
The Cap <xthecapx@gmail.com>
2018-01-08 08:58:43 -0500

    Fix ng serve

--- unitTestingIntegration/src/app/app.module.ts
+++ unitTestingIntegration/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { VoterComponent } from './1-voter/voter.component';
 
 import { routes } from './app.routes';
 import { UsersComponent } from './users/users.component';
-import { NavComponent } from './nav/nav.component';
 import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
@@ -23,7 +22,6 @@ import { HighlightDirective } from './highlight.directive';
     UserDetailsComponent,
     VoterComponent,
     UsersComponent,
-    NavComponent,
     HighlightDirective
   ],
   imports: [
