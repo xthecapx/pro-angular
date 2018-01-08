343f0aa10836ef5815e2ad2bd35ce08e3c687c3d
The Cap <xthecapx@gmail.com>
2018-01-08 09:08:33 -0500

    Basic configuration for integration

--- unitTestingIntegration/src/app/1-voter/voter.component.spec.ts
+++ unitTestingIntegration/src/app/1-voter/voter.component.spec.ts
@@ -1,8 +1,22 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
 
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
   beforeEach(() => {
+    // new VoterComponent(); // Unit Test mode
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement // Root Element of the component
+    // fixture.debugElement // Wrapper around native element
   });
 
   it('', () => {
