e5d68cd36bd90729c2edefae6d803443bd487471
The Cap <xthecapx@gmail.com>
2018-01-08 12:07:40 -0500

    Testing RouterOutlet components

--- unitTestingIntegration/src/app/app.component.spec.ts
+++ unitTestingIntegration/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule.withRoutes([]) ],
       declarations: [ AppComponent ]
     })
       .compileComponents();
@@ -20,5 +22,22 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('Should have a router outlet', () => {
+    let de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(de).not.toBeNull();
+  });
+
+  it('Should have a link to todos page', () => {
+    let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+    // <a href="/todos">
+    let index = debugElements.findIndex(de => {
+      return de.properties['href'] === '/todos';
+    });
+
+    expect(index).toBeGreaterThan(-1);
   });
 });
