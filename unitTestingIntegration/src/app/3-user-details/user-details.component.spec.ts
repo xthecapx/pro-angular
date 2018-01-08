4039eaaf9cd0edcec0f7fd5470ba3b54a6810fa7
The Cap <xthecapx@gmail.com>
2018-01-08 11:48:10 -0500

    Exercise 03 Dealing with route params

--- unitTestingIntegration/src/app/3-user-details/user-details.component.spec.ts
+++ unitTestingIntegration/src/app/3-user-details/user-details.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 import { UserDetailsComponent } from './user-details.component';
 
@@ -12,7 +12,17 @@ class RouterStub {
 }
 
 class ActivedRouteStub {
-  params: Observable<any> = Observable.empty();
+  private subject = new Subject();
+
+  // params: Observable<any> = Observable.empty();
+
+  get params() {
+    return this.subject.asObservable();
+  }
+
+  push(value) {
+    this.subject.next(value);
+  }
 }
 
 describe('UserDetailsComponent', () => {
@@ -53,6 +63,16 @@ describe('UserDetailsComponent', () => {
     component.save();
 
     expect(spy).toHaveBeenCalledWith(['users']);
+  });
+
+  it('Should navigate the user to the not found page when invalid user id is pass', () => {
+    let router = TestBed.get(Router);
+    let spy = spyOn(router, 'navigate');
+
+    let route: ActivedRouteStub = TestBed.get(ActivatedRoute);
+    route.push({ id: 0 });
+
+    expect(spy).toHaveBeenCalledWith(['not-found']);
   });
 });
 
