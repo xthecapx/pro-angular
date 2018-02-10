34b4992a0177ec7d9e767762f22f1b7e46aa4bc8
The Cap <xthecapx@gmail.com>
2018-02-10 17:03:09 -0500

    Chapter 14 (Page 332): Generating the Elements from the Model

--- events-and-forms/src/app/product.model.ts
+++ events-and-forms/src/app/product.model.ts
@@ -1,8 +1,3 @@
 export class Product {
-    
-        constructor(public id?: number,
-                    public name?: string,
-                    public category?: string,
-                    public price?: number) {}
-    }
-    
+  constructor(public id?: number, public name?: string, public category?: string, public price?: number) {}
+}
--- events-and-forms/src/app/repository.model.ts
+++ events-and-forms/src/app/repository.model.ts
@@ -1,10 +1,10 @@
-import { Product } from "./product.model";
-import { SimpleDataSource } from "./datasource.model";
+import { Product } from './product.model';
+import { SimpleDataSource } from './datasource.model';
 
 export class Model {
   private dataSource: SimpleDataSource;
   private products: Product[];
-  private locator = (p: Product, id: number) => p.id == id;
+  private locator = (p: Product, id: number) => p.id === id;
 
   constructor() {
     this.dataSource = new SimpleDataSource();
@@ -21,17 +21,17 @@ export class Model {
   }
 
   saveProduct(product: Product) {
-    if (product.id == 0 || product.id == null) {
+    if (product.id === 0 || product.id == null) {
       product.id = this.generateID();
       this.products.push(product);
     } else {
-      let index = this.products.findIndex(p => this.locator(p, product.id));
+      const index = this.products.findIndex(p => this.locator(p, product.id));
       this.products.splice(index, 1, product);
     }
   }
 
   deleteProduct(id: number) {
-    let index = this.products.findIndex(p => this.locator(p, id));
+    const index = this.products.findIndex(p => this.locator(p, id));
     if (index > -1) {
       this.products.splice(index, 1);
     }
