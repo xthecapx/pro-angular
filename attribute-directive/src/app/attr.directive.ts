6215b9af7379406e5f7fe07143076cffaf3fa409
The Cap <xthecapx@gmail.com>
2018-02-18 20:40:31 -0500

    Chapter 15 (Page 349): Responding to Input Property Changes

--- attribute-directive/src/app/attr.directive.ts
+++ attribute-directive/src/app/attr.directive.ts
@@ -1,17 +1,33 @@
-import { Directive, ElementRef, Attribute, Input } from '@angular/core';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import {
+  Directive,
+  ElementRef,
+  Attribute,
+  Input,
+  SimpleChange
+} from '@angular/core';
+import { OnInit, OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
   selector: '[pa-attr]'
 })
-export class PaAttrDirective implements OnInit {
+export class PaAttrDirective implements OnChanges {
   // tslint:disable-next-line:no-input-rename
   @Input('pa-attr') bgClass: string;
 
   constructor(private element: ElementRef) {}
 
-  ngOnInit() {
-    this.element.nativeElement.classList.add(this.bgClass || 'bg-success');
+  ngOnChanges(changes: { [property: string]: SimpleChange }): void {
+    const change = changes['bgClass'];
+    const classList = this.element.nativeElement.classList;
+
+    console.log(JSON.stringify(change));
+
+    if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+      classList.remove(change.previousValue);
+      classList.add(change.currentValue);
+    } else {
+      classList.add(change.currentValue);
+    }
   }
 }
