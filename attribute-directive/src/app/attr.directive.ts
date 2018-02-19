0c0e80a861239bb2a5db1d5bc45d6fdc88fb99b1
The Cap <xthecapx@gmail.com>
2018-02-18 20:13:17 -0500

    Chapter 15 (Page 346): Creating Data-Bound input properties

--- attribute-directive/src/app/attr.directive.ts
+++ attribute-directive/src/app/attr.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef, Attribute } from '@angular/core';
+import { Directive, ElementRef, Attribute, Input } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
   selector: '[pa-attr]'
 })
-export class PaAttrDirective {
-  constructor(
-    element: ElementRef,
-    @Attribute('pa-attr-class') bgClass: string
-  ) {
-    element.nativeElement.classList.add(bgClass || 'bg-success');
+export class PaAttrDirective implements OnInit {
+  // tslint:disable-next-line:no-input-rename
+  @Input('pa-attr') bgClass: string;
+
+  constructor(private element: ElementRef) {}
+
+  ngOnInit() {
+    this.element.nativeElement.classList.add(this.bgClass || 'bg-success');
   }
 }
